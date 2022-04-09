cf3cebe8cbd85d1d28832df6f453dd5c74a44a75
Flavio Siotto <flavio.siotto@gmail.com>
2022-04-09 21:50:05 +0200

    first commit

---
+++ binance/src/main/java/com/github/BinanceSourceConnector.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.utils.AppInfoParser;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+public class BinanceSourceConnector extends SourceConnector {
+
+    private static final ConfigDef CONFIG_DEF = new ConfigDef();
+
+    @Override
+    public String version() {
+        return AppInfoParser.getVersion();
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return BinanceSourceTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        ArrayList<Map<String, String>> configs = new ArrayList<>();
+        return configs;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+
+}
---
+++ binance/src/main/java/com/github/BinanceSourceTask.java
@@ -0,0 +1,77 @@
+package com.example;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.common.errors.InterruptException;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.source.SourceTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * BinanceSourceTask reads from Binance API.
+ */
+public class BinanceSourceTask extends SourceTask {
+    private static final Logger log = LoggerFactory.getLogger(BinanceSourceTask.class);
+
+    @Override
+    public String version() {
+        return new BinanceSourceConnector().version();
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+
+    }
+
+    @Override
+    public List<SourceRecord> poll() throws InterruptException {
+        return null;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+
+}
+
+/*
+    private static volatile boolean isTradeStreamUp = false;
+    private static WebSocketCallback onOpenCallback;
+    private static WebSocketCallback onMessageCallback;
+    private static WebSocketCallback onClosingCallback;
+    private static WebSocketCallback onFailureCallback;
+
+    public static void main(String[] args) {
+        WebsocketClientImpl client = new WebsocketClientImpl();
+
+        onOpenCallback = openEvent -> {
+            isTradeStreamUp = true;
+        };
+        onMessageCallback = (message) -> {
+            System.out.println(message);
+            client.closeAllConnections();
+        };
+        onClosingCallback = closingEvent -> {
+            isTradeStreamUp = false;
+        };
+        onFailureCallback = failureEvent -> {
+            isTradeStreamUp = false;
+            connectToTradeStream(client, onOpenCallback, onMessageCallback, onClosingCallback, onClosingCallback);
+        };
+        connectToTradeStream(client, onOpenCallback, onMessageCallback, onClosingCallback, onFailureCallback);
+    }
+
+    private static void connectToTradeStream(
+            WebsocketClientImpl client,
+            WebSocketCallback onOpenCallback,
+            WebSocketCallback onMessageCallback,
+            WebSocketCallback onClosingCallback,
+            WebSocketCallback onFailureCallback) {
+        client.tradeStream("btcusdt", onOpenCallback, onMessageCallback, onClosingCallback, onFailureCallback);
+    }
+    */
